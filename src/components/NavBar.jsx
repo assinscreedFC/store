e967f40ae46fa11011ff1da0b0b1b522020ad89b
ANIS <119749586+assinscreedFC@users.noreply.github.com>
2024-07-05 16:25:54 +0100

    :)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,6 @@
 import { NavLink } from "react-router-dom"
+import { CiShoppingCart } from "react-icons/ci";
+import { IconContext } from "react-icons";
 
 function NavBar(){
     return(
@@ -11,6 +13,20 @@ function NavBar(){
                     <li><NavLink to="/store">Back</NavLink></li>
                 </ul>
             </nav>
+            <div className="flex items-center gap-4 mx-4">
+            <div className="flex items-end flex-col">
+            <span class="relative flex  h-3 w-3">
+                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75 text-black"></span>
+                <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                </span>
+            <IconContext.Provider value={{ color: "blue" ,size:" 2em" }}>
+            <CiShoppingCart  />
+            </IconContext.Provider>
+            </div>
+           
+            
+            <button className="w-20 bg-zinc-900 text-zinc-100 p-3 rounded-md">Login</button>
+            </div>
         </header>
         </>
     )
